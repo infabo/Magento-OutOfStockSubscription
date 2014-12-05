d8bdcf0212c610f37941ca2c19b01297c02feb19
Ingo Fabbri <i.fabbri@newtown.at>
2014-12-05 16:52:37 +0100

    fix table prefix

--- app/code/local/BusinessKing/OutofStockSubscription/Model/Mysql4/Subscriber/Collection.php
+++ app/code/local/BusinessKing/OutofStockSubscription/Model/Mysql4/Subscriber/Collection.php
@@ -19,7 +19,7 @@ class BusinessKing_OutofStockSubscription_Model_Mysql4_Subscriber_Collection ext
     { 
     	parent::_initSelect();
 		//$attributeId = Mage::getResourceSingleton('outofstocksubscription/info')->getAttributeId();
-    	$this->getSelect()->join(array("cpe" => 'catalog_product_entity'), 'main_table.product_id=cpe.entity_id', array('sku'=>'cpe.sku'));
+    	$this->getSelect()->join(array("cpe" => Mage::getSingleton('core/resource')->getTableName('catalog_product_entity')), 'main_table.product_id=cpe.entity_id', array('sku'=>'cpe.sku'));
     	//$this->getSelect()->join(array("cpev" => 'catalog_product_entity_varchar'), 'cpe.entity_id=cpev.entity_id', array('value'=>'cpev.value'));
     	//$this->getSelect()->where("cpev.attribute_id=".$attributeId);
     	
